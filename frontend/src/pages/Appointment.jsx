bd1e9bc45562d147d3c601c7da53056aca60f3e9
ZafarByte <zafargayas3@gmail.com>
2025-06-27 20:16:39 +0530

    Display doctor details on Appointment page
    Fetches and displays detailed information about a doctor based on the route parameter. Uses context to access doctor data and updates the UI with the selected doctor's image, name, degree, specialty, experience, and about section.

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import{useParams} from 'react-router-dom'
+import {AppContext} from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const Appointment = () => {
+  const {docId} = useParams()
+  const {doctors}=useContext(AppContext)
+  const[docInfo,setDocInfo]= useState(null)
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc=>doc._id === docId)
+    setDocInfo(docInfo)
+    console.log(docInfo)
+  }
+
+  useEffect(()=>{
+    fetchDocInfo()
+  },[doctors,docId])
+
   return (
-    <div>Appointment</div>
+    <div>
+      {/*------------------Doctor details--------------------------*/}
+      <div>
+        <div>
+          <img src={docInfo.image} alt="" />
+        </div>
+        <div>
+          {/*------------------Doc Info--------------------------*/}
+          <p>{docInfo.name} <img src={assets.verified_icon} alt="" /></p>
+          <div>
+            <p>{docInfo.degree} - {docInfo.speciality}</p>
+            <button>{docInfo.experience}</button>
+          </div>
+
+          <div>
+            <p>About <img src={assets.info_icon} alt="" /></p>
+            <p>{docInfo.about}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
