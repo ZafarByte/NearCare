2674a63cb2f7eebe007ae827b1bade1222cbf7b9
ZafarByte <zafargayas3@gmail.com>
2025-06-30 19:42:59 +0530

    Update Appointment.jsx

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -36,12 +36,12 @@ const Appointment = () => {
               <p className='text-sm text-gray-700 max-w-[700px] mt-1'>{docInfo.about}</p>
             </div>
             <p className='text-gray-500 font-medium mt-4'>
-              Appointment fee: <span className='text-gray-600'>${docInfo.fees}</span>
+              Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
             </p>
           </div>
         </div>
       ) : (
-        <p>Loading doctor information...</p>
+        <p>Loading..</p>
       )}
     </div>
   )
