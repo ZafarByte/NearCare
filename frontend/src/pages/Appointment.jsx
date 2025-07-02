c2702eea8a1df25d1e3c7902a81b37533dcdebac
ZafarByte <zafargayas3@gmail.com>
2025-07-02 16:21:31 +0530

    Add horizontal scroll for doctor slot times
    Introduced a horizontally scrollable section displaying available doctor slot times below the existing content. This enhances the UI by allowing users to view and select from multiple time slots more easily.

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -114,6 +114,14 @@ const Appointment = () => {
                 ))
               }
             </div>
+
+            <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+              {docSlots.length && docSlots[slotIndex].map((item,index)=>(
+                <p className={`text-sm font-light flex-shrink-0 px-5 py-2  rounded-full cursor-pointer`} key={index}>
+                  {item.time.toLowerCase()}
+                </p>
+              ))}
+            </div>
           </div>
     </div>
   )
