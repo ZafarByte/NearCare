1d6eafa5b9f6e1a68b025334dd383b0682cc9d1c
ZafarByte <zafargayas3@gmail.com>
2025-07-03 22:10:57 +0530

    Add RelatedDoctors component and enhance About page
    Introduces the RelatedDoctors component to display doctors with the same speciality, and integrates it into the Appointment page. Expands the About page with detailed content about the platform and reasons to choose the service.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500 '>
+        <p >ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-12 '>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 w md:w-2/4 text-sm text-gray-600'>
+          <p>
+            Welcome to NearCare – Your Neighborhood Health Companion
+            NearCare is your trusted partner in healthcare, bringing essential medical services closer to your home. We are committed to simplifying the way you access quality care by connecting you with verified doctors, clinics, and health professionals in your local area. Whether you need a general check-up, specialist consultation, or home-based care, NearCare makes it easy to find and book the right services with just a few clicks.
+          </p>
+          <p>
+            At NearCare, we understand the importance of timely medical attention and personalized support. Our platform is designed to offer seamless appointment scheduling, transparent reviews, and real-time availability of healthcare providers near you. We bridge the gap between patients and healthcare professionals, ensuring you receive the care you need when you need it — without the long waits or travel hassles.
+          </p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>
+            Our mission is to empower communities by making healthcare more accessible, efficient, and people-centric. Whether you’re looking for primary care, pediatricians, dental services, or wellness experts, NearCare is here to guide you every step of the way. With our user-friendly platform and community-driven approach, we’re redefining what it means to care — right in your neighborhood.
+          </p>
+        </div>
+      </div>
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#0f172a] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#0f172a] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convenience:</b>
+          <p>Aceess to a network of entrusted healthcare professionals in your area.</p>
+        </div>
+        <div className='border px-10 md:px-16 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#0f172a] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Tailored recommendation and reminder to help you stay on top of your health.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointment = () => {
   const { docId } = useParams()
@@ -18,6 +19,7 @@ const Appointment = () => {
     setDocInfo(doc)
   }
 
+  
   const getAvailableSlot=async ()=>{
     setDocSlots([])
     // getting current date
@@ -117,12 +119,17 @@ const Appointment = () => {
 
             <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
               {docSlots.length && docSlots[slotIndex].map((item,index)=>(
-                <p className={`text-sm font-light flex-shrink-0 px-5 py-2  rounded-full cursor-pointer`} key={index}>
+                <p onClick={()=>setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2  rounded-full cursor-pointer ${item.time===slotTime ?'bg-[#0f172a] text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
                   {item.time.toLowerCase()}
                 </p>
               ))}
             </div>
+
+            <button className='bg-[#0f172a] text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an Appointment</button>
           </div>
+
+          {/* Listing Related Doctors*/}
+          <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
     </div>
   )
 }
