47d7f13fde684653b1bcc765f3d249deb250b481
ZafarByte <zafargayas3@gmail.com>
2025-07-24 23:36:03 +0530

    Enhance sidebar styling and add doctor form UI
    Sidebar navigation links now have improved styling with active state highlighting. The AddDoctor page has been updated to include a form for adding a doctor, including fields for name, email, password, and an image upload area.

--- admin/src/component/Sidebar.jsx
+++ admin/src/component/Sidebar.jsx
@@ -11,23 +11,24 @@ import { assets } from "../assets/assets";
     return (
       <div className="min-h-screen bg-white border-r">
         {   
-            aToken && <ul>
-                <NavLink to={'/admin-dashboard'}>
+            aToken && <ul className="text-[#515151] ">
+
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4  border-[#0f172a]': ''}`} to={'/admin-dashboard'}>
                     <img src={assets.home_icon} alt="" />
                     <p>Dashboard</p>
                 </NavLink>
 
-                <NavLink to={'/all-appointments'}>
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4  border-[#0f172a]': ''}`} to={'/all-appointments'}>
                     <img src={assets.appointment_icon} alt="" />
                     <p>Appointment</p>
                 </NavLink>
 
-                <NavLink to={'/add-doctor'}>
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4  border-[#0f172a]': ''}`} to={'/add-doctor'}>
                     <img src={assets.add_icon} alt="" />
                     <p>Add Doctor</p>
                 </NavLink>
 
-                <NavLink to={'/doctor-lists'}>
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4  border-[#0f172a]': ''}`} to={'/doctor-lists'}>
                     <img src={assets.people_icon} alt="" />
                     <p>Doctors List</p>
                 </NavLink>
--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,11 +1,59 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const AddDoctor =()=>
 {
     return (
-        <div>
-            AddDoctor
-        </div>
+        <form action="">
+            <p>Add Doctor</p>
+            <div>
+                <div>
+                <label htmlFor="doc-img">
+                    <img src={assets.upload_area} alt="" />
+
+                </label>
+                <input type="file" id="doc-img" hidden/>
+                <p>Upload Doctor <br /> Picture</p>
+                </div>
+
+                <div>
+                    <div>
+                        <div>
+                            <p>Your Name</p>
+                            <input type="text" placeholder="Name" required />
+                        </div>
+
+                        <div>
+                            <p>Doctor Email</p>
+                            <input type="email" placeholder="Email" required />
+                        </div>
+                        <div>
+                            <p>Doctor Password</p>
+                            <input type="text" placeholder="Password" required />
+                        </div>
+                        <div>
+                            <p>Experience</p>
+                            <select name="" id="">
+                                <option value="1 Year">1 Year</option>
+                                <option value="2 Year">2 Year</option>
+                                <option value="3 Year">3 Year</option>
+                                <option value="4 Year">4 Year</option>
+                                <option value="5 Year">5 Year</option>
+                                <option value="6 Year">6 Year</option>
+                                <option value="7 Year">7 Year</option>
+                                <option value="8 Year">8 Year</option>
+                                <option value="9 Year">9 Year</option>
+                                <option value="10 Year">10 Year</option>
+                            </select>
+                        </div>
+                        <div>
+                            <p>Doctor Password</p>
+                            <input type="Number" placeholder="Fee" required />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     )
 }
 
