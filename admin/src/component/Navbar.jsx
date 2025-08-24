657ace8282cfcfcf25ab22c3b328061b38adc60f
Zafar Gayas <164781713+ZafarByte@users.noreply.github.com>
2025-08-24 22:25:41 +0530

    Update Navbar.jsx

--- admin/src/component/Navbar.jsx
+++ admin/src/component/Navbar.jsx
@@ -4,27 +4,36 @@ import { AdminContext } from "../context/AdminContext"
 import { useNavigate } from "react-router-dom"
 import { DoctorContext } from "../context/DoctorContext"
 
-const Navbar=()=>{
-    const {aToken,setAToken} = useContext(AdminContext)
-    const {dToken,setDToken} = useContext(DoctorContext)
-    const navigate=useNavigate()
+const Navbar = () => {
+    const { aToken, setAToken } = useContext(AdminContext)
+    const { dToken, setDToken } = useContext(DoctorContext)
+    const navigate = useNavigate()
 
-    const logout = () =>{
+    const logout = () => {
         navigate('/')
         aToken && setAToken('')
         aToken && localStorage.removeItem('aToken')
         dToken && setDToken('')
         dToken && localStorage.removeItem('dToken')
     }
-    return(
-        <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
-          <div className="flex items-center gap-2 text-xs">
-            <img className="w-36 sm:w-40 cursr-pointer"  src={assets.logo} alt="" />
-            <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
-                {aToken ? 'Admin Panel' : 'Doctor'}
-            </p>
-          </div>
-          <button onClick={logout}  className="bg-[#0f172a] text-white text-sm px-10 py-2 rounded-full cursor-pointer">Logout</button>
+    return (
+        <div className="flex flex-col sm:flex-row justify-between items-center px-2 sm:px-10 py-2 sm:py-3 border-b bg-white gap-2 sm:gap-0">
+            <div className="flex items-center gap-2 text-xs w-full sm:w-auto justify-between sm:justify-start">
+                <img
+                    className="w-28 sm:w-40 cursor-pointer"
+                    src={assets.logo}
+                    alt=""
+                />
+                <p className="border px-2 py-0.5 rounded-full border-gray-500 text-gray-600 text-xs sm:text-sm">
+                    {aToken ? 'Admin Panel' : 'Doctor'}
+                </p>
+            </div>
+            <button
+                onClick={logout}
+                className="bg-[#0f172a] text-white text-xs sm:text-sm px-6 sm:px-10 py-2 rounded-full cursor-pointer w-full sm:w-auto"
+            >
+                Logout
+            </button>
         </div>
     )
 }
